9c68732f266cf5d5ffe5eda76d5e9ab26bb20535
oluwatobiHammed <oladipupo_tobi@yahoo.com>
2021-04-24 13:09:22 +0100

    Dev tool initial commit

---
+++ oluwatobi_view_tool/lib/oluwatobi_view_tool.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "oluwatobi_view_tool/version"
+require_relative "oluwatobi_view_tool/renderer"
+module OluwatobiViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
